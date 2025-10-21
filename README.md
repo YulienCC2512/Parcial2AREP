bf710f651a8a991ea6db13683678d152ec736d1a
unknown <julian.cardenas-c@labinfo.is.escuelaing.edu.co>
2025-10-21 11:34:04 -0500

    README

---
+++ README.md
@@ -0,0 +1,30 @@
+# Parcial 2 AREP
+## Julian Santiago Cardenas
+
+
+--- 
+
+Requisitos de ejecucion
+- Maven
+- Java 17
+
+
+Herramientas utilizadas
+- AWS
+- Sprinboot
+- Java
+- Intellij
+
+Creacion de klas instanciass en AWS:
+![](images/instancias.png)
+![](images/prueba.png)
+
+Realizmos el reto matematico en Sprinboot usando java, los cuales se prueban localmente usando POSTMAN:
+![](images/compila.png)
+![](images/local.png)
+
+Iniciamos con la programacion de nuestro servidor de Proxy:
+![](images/proxy1.png)
+
+Subimmos utilizando git hub, y corremos la primera instancia:
+![](images/despliegue.png)
--- src/main/java/com/edu/eci/arep/mathservice/controllers/MathController.java
+++ src/main/java/com/edu/eci/arep/mathservice/controllers/MathController.java
@@ -19,25 +19,26 @@ public class MathController {
             }
         }
         factores.add(value);
-        return "{operacion:" + operacion + ", input : " + value + "output :" + factores +"}";
+        return "{operacion:" + operacion + ", input : " + value + " output :" + factores +"}";
     }
 
     @GetMapping("/primes")
     public String primes(@RequestParam Integer value){
         String operacion = "primos";
         ArrayList primos = new ArrayList();
-
-        for (int i = 1; i <= value;i++ ){
+        Integer counter = 0;
+        for (int i = 1; i <= value;i++){
             ArrayList factores = new ArrayList();
             for(int j = 1; j <= value/2; j++){
                 if (value % j == 0){
                     factores.add(j);
                 }
             }
-            if (factores.size() == 2){
-                primos.add(value);
+            if (factores.size() < 2){
+                primos.add(i);
             }
         }
+
 
         return "{operacion:" + operacion + ", input : " + value + " output :" + primos +"}";
     }
